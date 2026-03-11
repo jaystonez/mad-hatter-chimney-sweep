bd250b9938ad4a75f6255e829cc1ecb83054d634
jaystonez <travelingjay@gmail.com>
2026-03-11 00:45:45 -0700

    Update contact number for leak inspection

--- app/resources/water-leaking-through-chimney/page.tsx
+++ app/resources/water-leaking-through-chimney/page.tsx
@@ -162,7 +162,7 @@ export default function WaterLeakingThroughChimneyPage() {
             href="/contact"
             className="bg-yellow-500 text-black font-bold px-8 py-4 rounded-lg text-lg hover:bg-yellow-400 transition inline-block"
           >
-            Schedule a Leak Inspection &mdash; (206) 555-0123
+            Schedule a Leak Inspection &mdash; (206) 274-6409
           </Link>
         </div>
       </section>
